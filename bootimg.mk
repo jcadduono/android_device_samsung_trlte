1a7d9040200f525daecf9a951c045d685d691f44
James Christopher Adduono <jc@adduono.com>
2016-06-11 18:36:21 -0400

    Import SM-N910F TWRP zImage and dtb.img (1.0)
    Change-Id: I950907c56a70a0b54284215ea338701daa5d7085

--- bootimg.mk
+++ bootimg.mk
@@ -2,21 +2,16 @@ MKBOOTIMG := device/samsung/trlte/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
-BUILT_RAMDISK_CPIO := $(PRODUCT_OUT)/ramdisk-recovery.cpio
-COMPRESS_COMMAND := xz --format=lzma --lzma1=dict=16MiB
-
 ifdef TARGET_PREBUILT_DTB
 	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
 endif
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk)
-	@echo "------- Compressing recovery ramdisk -------"
-	$(hide) $(COMPRESS_COMMAND) "$(BUILT_RAMDISK_CPIO)"
 	@echo "------- Making recovery image -------"
 	$(hide) $(MKBOOTIMG) \
 		--kernel $(TARGET_PREBUILT_KERNEL) \
-		--ramdisk $(BUILT_RAMDISK_CPIO).lzma \
+		--ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img \
 		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
 		--base $(BOARD_KERNEL_BASE) \
 		--pagesize $(BOARD_KERNEL_PAGESIZE) \
